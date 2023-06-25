bb05b8715bcc9fba6a21300c9ab57b6c24a46e00
Glottal Trill <philmslim@protonmail.com>
2023-06-25 17:52:00 -0400

    :fire: Removing unused function

--- filters.go
+++ filters.go
@@ -12,6 +12,7 @@ func clamp(v float64) uint8 {
 	return uint8(math.Min(math.Max(v, 0.0), 255.0) + 0.5)
 }
 
+// COMMENT: This might be better as a method on a struct
 func sigmoid(a, b, x float64) float64 {
 	return 1 / (1 + math.Exp(b*(a-x)))
 }
@@ -38,6 +39,7 @@ func ImageStripFilter(img image.Image) image.Image {
 // sigmoid function to simulate image cross processing, best results with midpoint: 0.5 and factor 10
 func CrossProcessingFilter(img image.Image, midpoint, factor float64) *image.NRGBA {
 
+	// TODO:  move these to a colours package?
 	red := make([]uint8, 256)
 	green := make([]uint8, 256)
 	blue := make([]uint8, 256)
@@ -45,6 +47,8 @@ func CrossProcessingFilter(img image.Image, midpoint, factor float64) *image.NRG
 	b := math.Abs(factor)
 	sig0 := sigmoid(a, b, 0)
 	sig1 := sigmoid(a, b, 1)
+
+	// TODO: Is there a existing library for Euler's number?
 	e := 1.0e-6
 
 	for i := 0; i < 256; i++ {
@@ -70,46 +74,6 @@ func CrossProcessingFilter(img image.Image, midpoint, factor float64) *image.NRG
 
 	fn := func(c color.NRGBA) color.NRGBA {
 		return color.NRGBA{red[c.R], green[c.G], blue[c.B], c.A}
-	}
-
-	return imaging.AdjustFunc(img, fn)
-}
-
-func xCrossProcessing(img image.Image, midpoint, factor float64) *image.NRGBA {
-
-	red := make([]uint8, 256)
-	green := make([]uint8, 256)
-	blue := make([]uint8, 256)
-	a := math.Min(math.Max(midpoint, 0.0), 1.0)
-	b := math.Abs(factor)
-	sig0 := sigmoid(a, b, 0)
-	sig1 := sigmoid(a, b, 1)
-	e := 1.0e-6
-
-	for i := 0; i < 256; i++ {
-		x := float64(i) / 255.0
-		sigX := sigmoid(a, b, x)
-		f := (sigX - sig0) / (sig1 - sig0)
-		red[i] = clamp(f * 255.0)
-	}
-
-	for i := 0; i < 256; i++ {
-		x := float64(i) / 255.0
-		sigX := sigmoid(a, b, x)
-		f := (sigX - sig0) / (sig1 - sig0)
-		green[i] = clamp(f * 255.0)
-	}
-
-	for i := 0; i < 256; i++ {
-		x := float64(i) / 255.0
-		arg := math.Min(math.Max((sig1-sig0)*x+sig0, e), 1.0-e)
-		f := a - math.Log(1.0/arg-1.0)/b
-		blue[i] = clamp(f * 255.0)
-	}
-
-	fn := func(c color.NRGBA) color.NRGBA {
-		//return color.NRGBA{blue[c.R], green[c.G], red[c.B], c.A}
-		return color.NRGBA{blue[c.R], green[c.G], blue[c.B], c.A}
 	}
 
 	return imaging.AdjustFunc(img, fn)
